a6b1c343280b3766e7b54519bf97d47294c375a4
shankha <shankhabanerjee@gmail.com>
2014-01-29 23:17:23 +0530

    setup fibonaaci

--- DynamicPrograaming.py
+++ DynamicPrograaming.py
@@ -1,20 +1,34 @@
 import sys
+import random
+
+def fibonacci():
+  #{
+	#nos = random.sample(range(100), 1)
+	#random.shuffle(nos)
+	nos = random.randint(1, 50)
+	print(nos)
+	fib2 = list(0:2)	
+	fib2[0] = 1
+	fib2[1] = 1
+#}
 
 def foo():
-#{
-	print ("Hello")
+  #{
+	#print ("Hello")
+	pass
 #}
 
 def main(argv=None):
-#{  
+  #{  
 	if argv is None:
-  		argv = sys.argv
+	  argv = sys.argv
 	foo()	
+	fibonacci()
 #}   
 
 
 if __name__ == "__main__":
-#{  
+  #{  
 	sys.exit(main())
 #}	
 
