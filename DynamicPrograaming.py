90651cc2f5d4fd628b701a489c5565d5412f4454
shankha <shankhabanerjee@gmail.com>
2014-01-30 18:00:01 +0530

    Adding cProfile changes

--- DynamicPrograaming.py
+++ DynamicPrograaming.py
@@ -8,18 +8,19 @@ def fibonacci():
   #{
 	#nos = random.sample(range(100), 1)
 	#random.shuffle(nos)
-	nos = random.randint(1, 50)
+	nos = random.randint(1, 10000)
 	#print(nos)
 	fib2 = [0, 1]	
-	fibnos = [None] * nos
+	nos = 120000
+	#fibnos = [None] * nos
 	#start = default_timer() 
-	start = time.clock()
+	start = time.time()
 	i = 0
 	while True:
 	  #{
 		fib = fib2[0] + fib2[1]
 		#print ('% iteration : %d\n' % (i, fib))
-		fibnos[i] = fib
+		#fibnos[i] = fib
 		fib2[1] = fib
 		fib2[0] = fib2[1]
 		i = i + 1
@@ -31,7 +32,7 @@ def fibonacci():
 	#stop = timeit.default_timer() 
 	#print (fibnos)
 	#print (stop - start)
-	print (time.time() - start)
+	print ('%d time = %f\n' % (nos, (time.time() - start)))
 #}
 
 def foo():
@@ -45,8 +46,8 @@ def main(argv=None):
 	if argv is None:
 	  argv = sys.argv
 	foo()	
-	#cProfile.run('fibonacci()')
 	fibonacci()
+	cProfile.run('fibonacci()')
 #}   
 
 
