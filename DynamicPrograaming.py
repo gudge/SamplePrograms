86e3f3f29fbf3e37c800de0f59c726414953b025
shankha <shankhabanerjee@gmail.com>
2014-01-30 10:09:20 +0530

    Basic Changes(1)

--- DynamicPrograaming.py
+++ DynamicPrograaming.py
@@ -2,24 +2,34 @@ import sys
 import random
 
 def fibonacci():
-  #{
+#{
 	#nos = random.sample(range(100), 1)
 	#random.shuffle(nos)
 	nos = random.randint(1, 50)
-	print(nos)
-	fib2 = list(0:2)	
-	fib2[0] = 1
-	fib2[1] = 1
+	#print(nos)
+	if (nos == 50):
+	#{  
+		return
+	#}  
+	fib2 = [0, 1]	
+	i = 0
+	while True:
+	#{
+    	if i == nos:
+        #{
+        	break
+		#}  
+	#}  
 #}
 
 def foo():
-  #{
+#{
 	#print ("Hello")
 	pass
 #}
 
 def main(argv=None):
-  #{  
+#{  
 	if argv is None:
 	  argv = sys.argv
 	foo()	
@@ -28,7 +38,7 @@ def main(argv=None):
 
 
 if __name__ == "__main__":
-  #{  
+#{  
 	sys.exit(main())
 #}	
 
