3eec8f02c4be3b8a43554f63b1c93f9e9495f160
shankha <shankhabanerjee@gmail.com>
2014-02-02 17:19:23 +0530

    Binomial

--- DynamicPrograaming.py
+++ DynamicPrograaming.py
@@ -4,6 +4,42 @@ import cProfile
 import timeit
 import time
 
+def binomial_coefficient():
+  #{
+	#   n          n - 1        n - 1
+	#         =             + 
+	#   k          k  - 1       k
+	nos = random.randint(1, 10000)
+	nos = 3000
+	# A two dimensional array allocation
+	#bc = [[ None ] * nos ] * nos
+	bc = [[ None ] * (nos + 1) ] * 2
+	bc[0][0] = 1
+	i = 1
+	start = time.time()
+	while True:
+	  #{
+		bc[0][i] = 1
+		j = 1
+		while True:
+		  #{
+			bc[1][j] ==  bc[0][j-1] + bc[0][j]
+			j = j + 1
+			if j == i + 1:
+			  #{
+				break
+			#}  
+		#}  
+		i += 1
+		if i == nos + 1:
+		  #{
+			break
+		#}
+	#}  
+	print (bc[1][nos])
+	print ('%d time = %f\n' % (nos, (time.time() - start)))
+#}
+
 def fibonacci():
   #{
 	#nos = random.sample(range(100), 1)
@@ -11,7 +47,7 @@ def fibonacci():
 	nos = random.randint(1, 10000)
 	#print(nos)
 	fib2 = [0, 1]	
-	nos = 120000
+	nos = 50000
 	#fibnos = [None] * nos
 	#start = default_timer() 
 	start = time.time()
@@ -25,7 +61,7 @@ def fibonacci():
 		fib2[0] = fib2[1]
 		i = i + 1
 		if i == nos:
-		#{
+		  #{
 			break
 		#}  
 	#}  
@@ -46,8 +82,10 @@ def main(argv=None):
 	if argv is None:
 	  argv = sys.argv
 	foo()	
-	fibonacci()
-	cProfile.run('fibonacci()')
+	#fibonacci()
+	#cProfile.run('fibonacci()')
+
+	binomial_coefficient()
 #}   
 
 
